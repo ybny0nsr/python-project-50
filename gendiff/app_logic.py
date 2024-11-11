fccbfe993a3bfc4796084ef28d8f31e83a78354d
ybny0nsr <oleg.tikhonoff@gmail.com>
2024-11-11 23:14:12 +0300

    general daily commit

--- gendiff/app_logic.py
+++ gendiff/app_logic.py
@@ -39,18 +39,41 @@ def parse_n_sort(template: dict) -> list:
     return result
 
 
-def generate_diff(file_1: str, file_2: str):
+def find_diffs_json(dict_1: dict, dict_2: dict) -> str:
+    result = '{\n'
+    offset = '  '
+    keys_1, keys_2 = set(dict_1.keys()), set(dict_2.keys())
+    diff_values = {key for key in keys_1 & keys_2 if
+                   dict_1[key] != dict_2[key]}
+    diff_plus = keys_2 - keys_1
+    diff_minus = keys_1 - keys_2
+    keys_list = sorted(list(keys_1 | keys_2))
+    for key in keys_list:
+        if key in diff_values:
+            result += f'{offset}- {key}: {dict_1[key]}\n'
+            result += f'{offset}+ {key}: {dict_2[key]}\n'
+        elif key in diff_plus:
+            result += f'{offset}+ {key}: {dict_2[key]}\n'
+        elif key in diff_minus:
+            result += f'{offset}- {key}: {dict_1[key]}\n'
+        else:
+            result += f'{offset}  {key}: {dict_1[key]}\n'
+    result += '}'
+    return result
 
+
+def generate_diff(file_1: str, file_2: str):
     if files_not_found(file_1, file_2):
         sys.exit(1)
 
     template_1 = read_file_contents(file_1)
     template_2 = read_file_contents(file_2)
+    print(type(template_1))
     if None in (template_1, template_2):
         sys.exit(1)
 
-    structure_1 = parse_n_sort(template_1)
-    structure_2 = parse_n_sort(template_2)
+    list_1 = parse_n_sort(template_1)
+    list_2 = parse_n_sort(template_2)
 
-    print(f'{template_1=}\n{structure_1=}\n')
-    print(f'{template_2=}\n{structure_2=}')
+    print(f'{template_1=}\n{list_1=}\n')
+    print(f'{template_2=}\n{list_2=}')
