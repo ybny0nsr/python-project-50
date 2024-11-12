574d3a8f24a65b362771ff5230c2ba65f0cdb271
ybny0nsr <oleg.tikhonoff@gmail.com>
2024-11-12 21:57:34 +0300

    4th phase 1st gen

--- gendiff/app_logic.py
+++ gendiff/app_logic.py
@@ -24,7 +24,8 @@ def files_not_found(*files: tuple) -> bool:
 
 def read_file_contents(file: str) -> dict:
     try:
-        return json.load(open(file))
+        with open(file, 'r') as f:
+            return json.load(f)
     except Exception:
         print(f'An error occurred while reading the file "{file}')
 
@@ -39,7 +40,15 @@ def parse_n_sort(template: dict) -> list:
     return result
 
 
-def find_diffs_json(dict_1: dict, dict_2: dict) -> str:
+def python_to_json(value):
+    match value:
+        case True: return 'true'
+        case False: return 'false'
+        case None: return 'null'
+        case _: return f'{value}'
+
+
+def compare_dicts(dict_1: dict, dict_2: dict) -> str:
     result = '{\n'
     offset = '  '
     keys_1, keys_2 = set(dict_1.keys()), set(dict_2.keys())
@@ -50,14 +59,14 @@ def find_diffs_json(dict_1: dict, dict_2: dict) -> str:
     keys_list = sorted(list(keys_1 | keys_2))
     for key in keys_list:
         if key in diff_values:
-            result += f'{offset}- {key}: {dict_1[key]}\n'
-            result += f'{offset}+ {key}: {dict_2[key]}\n'
+            result += f'{offset}- {key}: ' + python_to_json(dict_1[key]) + '\n'
+            result += f'{offset}+ {key}: ' + python_to_json(dict_2[key]) + '\n'
         elif key in diff_plus:
-            result += f'{offset}+ {key}: {dict_2[key]}\n'
+            result += f'{offset}+ {key}: ' + python_to_json(dict_2[key]) + '\n'
         elif key in diff_minus:
-            result += f'{offset}- {key}: {dict_1[key]}\n'
+            result += f'{offset}- {key}: ' + python_to_json(dict_1[key]) + '\n'
         else:
-            result += f'{offset}  {key}: {dict_1[key]}\n'
+            result += f'{offset}  {key}: ' + python_to_json(dict_1[key]) + '\n'
     result += '}'
     return result
 
@@ -66,14 +75,16 @@ def generate_diff(file_1: str, file_2: str):
     if files_not_found(file_1, file_2):
         sys.exit(1)
 
-    template_1 = read_file_contents(file_1)
-    template_2 = read_file_contents(file_2)
-    print(type(template_1))
-    if None in (template_1, template_2):
+    dict_1 = read_file_contents(file_1)
+    dict_2 = read_file_contents(file_2)
+
+    if None in (dict_1, dict_2):
         sys.exit(1)
 
-    list_1 = parse_n_sort(template_1)
-    list_2 = parse_n_sort(template_2)
+    # list_1 = parse_n_sort(dict_1)
+    # list_2 = parse_n_sort(dict_2)
+    #
+    # print(f'{template_1=}\n{list_1=}\n')
+    # print(f'{template_2=}\n{list_2=}')
 
-    print(f'{template_1=}\n{list_1=}\n')
-    print(f'{template_2=}\n{list_2=}')
+    print(compare_dicts(dict_1, dict_2))
