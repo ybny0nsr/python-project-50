c88f657871e00656dac6700a8173c44610124cfd
ybny0nsr <oleg.tikhonoff@gmail.com>
2024-11-24 00:03:29 +0300

    general daily commit 241123/2

--- tests/test_adjust_output.py
+++ tests/test_adjust_output.py
@@ -1,12 +1,17 @@
+import pytest
 from gendiff.app_logic import adjust_output
 
 
-def test_python_to_json():
-    assert adjust_output(False) == 'false'
-    assert adjust_output(True) == 'true'
-    assert adjust_output(None) == 'null'
-    assert adjust_output(123) == '123'
-    assert adjust_output(1.23) == '1.23'
-    assert adjust_output([1, 'abc', 3.14]) == "[1, 'abc', 3.14]"
-    assert adjust_output("hexlet") == "hexlet"
-    assert adjust_output({"a": 1, "b": 1.0}) == "{'a': 1, 'b': 1.0}"
+@pytest.mark.parametrize(("python_data", "like_json_view"),
+                         [(False, 'false'),
+                          (True, 'true'),
+                          (None, 'null'),
+                          (123, '123'),
+                          (1.23, '1.23'),
+                          ([1, 'abc', 3.14], "[1, 'abc', 3.14]"),
+                          ('hexlet', 'hexlet'),
+                          ({"a": 1, "b": 1.0}, "{'a': 1, 'b': 1.0}"),
+                          ],
+                         )
+def test_python_to_json(python_data, like_json_view):
+    assert adjust_output(python_data) == like_json_view
